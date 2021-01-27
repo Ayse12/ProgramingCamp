6f96fef467977e6100ed42a014cdac3dc192fefc
AYŞE İLHANLI <20162013027@std.nisantasi.edu.tr>
2021-01-27 13:35:25 +0300

    Add project files.

---
+++ PythonDenemeleri/PythonDenemeleri.py
@@ -0,0 +1,19 @@
+#Comment Line
+baslik = "Yemek Tarifleri" #string
+vade = 12 #integer
+faizOrani = 1.47 #float
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani))
+print("-------------------------")
+
+mesaj = "Hoş Geldiniz"
+CustomerName = "Ayşe"
+CustomerLastName = "İlhanlı"
+sonucMesaj = mesaj + " " + CustomerName + " " + CustomerLastName
+print(sonucMesaj)
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1 + sayi2)
